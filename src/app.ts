0d1bd6c45a1aceb77186f677a8f2501c659c47d3
root <root@DESKTOP-UE7169K>
2024-11-28 10:44:38 +0900

    :sparkles: [Feat] 에러 핸들러 추가

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
+import errorHandler from './middlewares/errHandler';
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3001, () => console.log('Server Started'));
