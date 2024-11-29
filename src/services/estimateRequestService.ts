e0523a2841a7a3273b306988f961a5cfd363e90a
root <root@DESKTOP-UE7169K>
2024-11-29 10:29:52 +0900

    :sparkles: [Feat] 견적 요청 API service 작성 아직 미완성이며 DB변경으로 인한 commit입니다.

---
+++ src/services/estimateRequestService.ts
@@ -0,0 +1,40 @@
+import estimateRequestRepository from '../repositories/estimateRequestRepository';
+import movingInfoRepository from '../repositories/movingInfoRepository';
+import userRepository from '../repositories/userRepository';
+import { CreateEstimateReq } from '../structs/estimateRequest-struct';
+import { estimateReqSelect } from './selerts/estimateRequsetSelect';
+import { movinginfoSelect } from './selerts/movingInfoSelect';
+import { userCustomerSelect } from './selerts/userSelect';
+
+async function createEstimateReq(userId: number, data: CreateEstimateReq) {
+  const user = await userRepository.findUniqueOrThrowtData({
+    where: { id: userId },
+    select: userCustomerSelect,
+  });
+
+  // 소비자 프로필 유무 확인
+  if (!user.Customer) {
+    const error = new Error('프로필을 등록 해주세요');
+    throw error;
+  }
+
+  // 이사 정보 생성
+  const { comment, ...rest } = data;
+  const movingInfo = await movingInfoRepository.createData({
+    data: rest,
+    select: movinginfoSelect,
+  });
+
+  // 견적 요청 생성
+  const estimateReq = await estimateRequestRepository.createData({
+    data: {
+      customerId: user.Customer.id,
+      movingInfoId: movingInfo.id,
+      comment,
+    },
+    select: estimateReqSelect,
+  });
+
+  return; 
+}
+
---
+++ src/services/selerts/customerSelect.ts
@@ -0,0 +1,6 @@
+export const customerSelect = {
+  id: true,
+  profileImage: true,
+  serviceType: true,
+  region: true,
+};
