238a2a14676ccce161bc57f552b4a47c27815b98
root <root@DESKTOP-UE7169K>
2024-12-02 09:46:41 +0900

    :sparkles: [Feat] 견적 요청 API route 작성

---
+++ src/routes/estimateRequestRoute.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { validateEstimateReq } from '../middlewares/validateData';
+import estimateRequestController from '../controllers/estimateRequestController';
+
+const estimateReqRouter = express.Router();
+
+estimateReqRouter
+  .route('/')
+  .post(validateEstimateReq, estimateRequestController.createEstimateReq); // 유저 인증 미들웨어 필요
