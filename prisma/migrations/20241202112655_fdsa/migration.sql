a6a6b446549bb6791e62259677dcd933bcd3966a
Jun5_surface <chdh1005@naver.com>
2024-12-06 10:37:32 +0900

    :wrench: [Chore] 불필요한 마이그레이션 파일 삭제 > 마이그레이션 파일 통일 및 추후에는 마이그레이션 파일 삭제하지 않고 관리하도록

--- prisma/migrations/20241202112655_fdsa/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `nickname` on table `mover` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "mover" ALTER COLUMN "nickname" SET NOT NULL;
