aeb601030c9d5596367ac002378b666611c829a3
Minsugar98 <118067539+Minsugar98@users.noreply.github.com>
2024-12-10 14:01:44 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,6 @@ const app = express();
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-app.use(cors());
 app.use(
     cors({
       origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
