ca28c2d7302674e148e5fb9b6589701295b104c9
root <root@DESKTOP-UE7169K>
2024-12-11 13:38:12 +0900

    :sparkles: [Feat] 유저-받았던 견적 리스트 조회 API controller 작성

---
+++ src/controllers/estimateController.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express';
+import estimateService from '../services/estimateService';
+
+async function findReceivedEstimateList(
+  req: Request<{ estimateRequestId: string }, {}, {}>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    if (
+      !req.user ||
+      typeof req.user === 'string' ||
+      typeof req.user.id !== 'number'
+    ) {
+      throw new Error('다시 시도해 주세요');
+    }
+    const { id: userId } = req.user;
+    const estimateReqId = parseInt(req.params.estimateRequestId);
+    const estimateList = await estimateService.findReceivedEstimateList(
+      userId,
+      estimateReqId
+    );
+    res.send(estimateList);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+export default {
+  findReceivedEstimateList,
+};
