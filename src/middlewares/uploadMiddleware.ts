c680530fecdfd56dca816308cc5e6f20aa48d25f
root <root@DESKTOP-UE7169K>
2025-01-10 13:51:41 +0900

    :bug: [Fix] s3관련 코드 수정

--- src/middlewares/uploadMiddleware.ts
+++ src/middlewares/uploadMiddleware.ts
@@ -21,7 +21,6 @@ export const uploadImage = (fieldName: string) => {
     storage: multerS3({
       s3,
       bucket: AWS_S3_BUCKET,
-      acl: 'public-read',
       key: (req, file, cb) => {
         cb(null, `profiles/${Date.now()}_${file.originalname}`);
       },
